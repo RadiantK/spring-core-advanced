049e49f484e552e7cc56230a1e5c25a942a29734
RadiantK <pps8853@gmail.com>
2022-10-11 22:17:00 +0900

    ThreadLocal을 사용한 동시성 문제 해결
    ThreadLocal을 사용한 ThreadLocalLogTrace클래스를 통해 동시성 문제를 해결하고 구현 클래스를 테스트 및 서버 에서 해결된 동시성 문제를 확인한다.

--- advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -2,14 +2,24 @@ package hello.advanced;
 
 import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
 
+    // ThreadLocal 사용 x
+/*
     @Bean
     public LogTrace logTrace() {
         return new FieldLogTrace();
+    }
+*/
+
+    // ThreadLocal 사용
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 }
