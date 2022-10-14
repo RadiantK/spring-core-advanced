f596188af227189fa8122f77c04bf4122c920400
RadiantK <pps8853@gmail.com>
2022-10-14 16:44:19 +0900

    빈 후처리기 테스트
    빈 후처리기를 적용하여 A객체가 생성되고 A객체가 빈으로 등록되기 전에 B클래스로 변경하여 등록한다.

---
+++ proxy/src/test/java/hello/proxy/postprocessor/BeanPostProcessorTest.java
@@ -0,0 +1,72 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Slf4j
+public class BeanPostProcessorTest {
+
+    @Test
+    void basicConfig() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
+
+        //beanA의 이름으로 B 객체가 빈으로 등록된다.
+        B a = applicationContext.getBean("beanA", B.class);
+        a.helloB();
+
+        //B는 빈으로 등록되지 않는다.
+        Assertions.assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> applicationContext.getBean(A.class))
+                .isInstanceOf(NoSuchBeanDefinitionException.class);
+    }
+
+    @Configuration
+    static class BeanPostProcessorConfig {
+
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor postProcessor() {
+            return new AToBPostProcessor();
+        }
+    }
+
+    static class A {
+
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    static class B {
+
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+
+    static class AToBPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={} bean={}", beanName, bean);
+            if (bean instanceof A) {
+                return new B();
+            }
+
+            return bean;
+        }
+    }
+}
